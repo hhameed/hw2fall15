1743eb5fab68026e9f6afc50e2412e84fa15feb1
hhameed <haider-hameed@uiowa.edu>
2015-09-18 11:22:55 +0000

    part5 done with test

--- lib/part5.rb
+++ lib/part5.rb
@@ -1,6 +1,24 @@
 class CartesianProduct
   include Enumerable
 
-  # YOUR CODE HERE
+  def initialize arr1, arr2
+    @arr1 = arr1
+    @arr2 = arr2
+  end
+  
+  def each
+    @arr1.each do |i|
+      @arr2.each do |j|
+        puts "I am called...\n"
+        yield([i, j])
+      end
+    end
+  end
 
 end
+
+c = CartesianProduct.new([:a,:b], [4,5])
+c.each { |elt| puts elt.inspect }
+
+c = CartesianProduct.new([:a,:b], [])
+c.each { |elt| puts elt.inspect }
