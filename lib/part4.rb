fbb60b82ecdb3260c0d3a183083be76cf2774207
hhameed <haider-hameed@uiowa.edu>
2015-09-25 15:34:06 +0000

    Fixed the issue in assignment

--- lib/part4.rb
+++ lib/part4.rb
@@ -2,16 +2,15 @@ class Class
   def attr_accessor_with_history(attr_name)
     attr_name = attr_name.to_s   # make sure it's a string
     attr_reader attr_name        # create the attribute's getter
-    attr_reader attr_name+"_history" # create bar_history getter
     self.class_eval %Q[
-      def initialize
-        @#{attr_name}_history = [nil]
+      def #{attr_name}_history
+        @#{attr_name}_history = [nil] if !@#{attr_name}_history
+        @#{attr_name}_history
       end
       def #{attr_name}=(value)
         @#{attr_name} = value
-        @#{attr_name}_history << value
+        #{attr_name}_history << value
       end
     ]
   end
 end
-
